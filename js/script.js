00ae6076d1a9c305b1360e3f32f7a6d6acafb7fc
cindy wambui <montanacindy5@gmail.com>
2019-08-18 17:57:48 +0300

    Add initial function to validate the options

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function validayForm () {
+    var day = documeny.getElementById("day").value;
+    var month = document.getElemenyById("month").value;
+    var year = document.getElementById("year").value;
+    var gender = documrnt.getElementsByName("gender");
+}
