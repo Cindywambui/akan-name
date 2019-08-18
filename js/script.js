b16fb7a56ba0ed39fae4c3399603e701c7ad7d81
cindy wambui <montanacindy5@gmail.com>
2019-08-18 18:45:55 +0300

    Add changes to error input on the month

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-function validayForm () {
-    var day = documeny.getElementById("day").value;
-    var month = document.getElemenyById("month").value;
-    var year = document.getElementById("year").value;
-    var gender = documrnt.getElementsByName("gender");
-}
+
+    function validateForm() {
+        var date = document.getElementById('day');
+        var month = document.getElementById('month');
+        var year = document.getElementById('year');
+        var gender = document.getElementsByName('gender');
+    
+    
+        //validating the form inputs
+        if (date.value > 31 || date.value == null || date.value == "") {
+            document.getElementById('errorInput').innerHTML = "Wrong Date";
+            document.getElementById('errorInput').style.color = "red";
+            return false;
+        }
+        if(month.value > 12 || month.value == null || month.value == ""){
+            document.getElementById('errorInput').innerHTML = "Wrong Month";
+            document.getElementById('errorInput').style.color = "red";
+            return false;
+        }
+
